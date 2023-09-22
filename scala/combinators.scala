8029635d7d679a1945da60315bb19fe6cf6dc48a
Kyle Dewey <kyle.dewey@csun.edu>
2023-09-22 15:58:53 -0700

    added scala stuff for nondeterministic parsers

---
+++ scala/combinators.scala
@@ -0,0 +1,88 @@
+case class ~[+A, +B](_1: A, _2: B)
+
+trait Combinators {
+  import scala.language.implicitConversions
+
+  type Elem
+
+  // deterministic
+  // type Parser[A] = (List[Elem]) => Either[String, (A, List[Elem])]
+
+  // nondeterministic
+  // type Parser[A] = (List[Elem]) => Iterator[(A, List[Elem])]
+
+  trait Parser[+A] extends (List[Elem] => Iterator[(A, List[Elem])]) {
+    self =>
+
+    def first(tokens: List[Elem]): Option[A] = {
+      self.apply(tokens).collectFirst({ case (a, Nil) => a })
+    }
+
+    def ~[B](pRaw: => Parser[B]): Parser[~[A, B]] = {
+      tokens1 => {
+        lazy val p = pRaw
+        for {
+          (a, tokens2) <- self.apply(tokens1)
+          (b, tokens3) <- p.apply(tokens2)
+        } yield (new ~(a, b), tokens3)
+      }
+    }
+
+    def |[B >: A](pRaw: => Parser[B]): Parser[B] = {
+      tokens => {
+        // ++ for iterators already uses call-by-name...nice
+        self.apply(tokens) ++ pRaw.apply(tokens)
+      }
+    }
+
+    def map[B](f: A => B): Parser[B] = {
+      tokens1 => {
+        self.apply(tokens1).map({ case (a, tokens2) =>
+          (f(a), tokens2)
+        })
+      }
+    }
+
+    def ^^[B](f: A => B): Parser[B] = map(f)
+
+    def flatMap[B](f: A => Parser[B]): Parser[B] = {
+      tokens1 => {
+        self.apply(tokens1).flatMap({ case (a, tokens2) =>
+          f(a).apply(tokens2)
+        })
+      }
+    }
+  }
+
+  def success[A](a: A): Parser[A] = {
+    tokens => {
+      Iterator((a, tokens))
+    }
+  }
+
+  def failure: Parser[Nothing] = {
+    _ => Iterator()
+  }
+
+  def accept[A](pf: PartialFunction[Elem, A]): Parser[A] = {
+    tokens => {
+      tokens match {
+        case head :: tail if pf.isDefinedAt(head) => Iterator((pf(head), tail))
+        case _ => Iterator()
+      }
+    }
+  }
+
+  implicit def elem(e: Elem): Parser[Elem] = {
+    tokens => {
+      tokens match {
+        case (`e` :: tail) => Iterator((e, tail))
+        case _ => Iterator()
+      }
+    }
+  }
+
+  def opt[A](p: => Parser[A]): Parser[Option[A]] = {
+    p.map(a => Some(a)) | success(None)
+  }
+}
---
+++ scala/test.scala
@@ -0,0 +1,21 @@
+// exp ::= INTEGER | `(` `+` exp exp `)`
+
+sealed trait Token
+case class IntToken(i: Int) extends Token
+case object LeftParenToken extends Token
+case object PlusToken extends Token
+case object RightParenToken extends Token
+
+sealed trait Exp
+case class IntLiteralExp(i: Int) extends Exp
+case class PlusExp(left: Exp, right: Exp) extends Exp
+
+object Parser extends Combinators {
+  override type Elem = Token
+
+  lazy val intP: Parser[Int] = accept({ case IntToken(i) => i })
+  lazy val expP: Parser[Exp] = {
+    intP.map(IntLiteralExp.apply _) |
+    (LeftParenToken ~ PlusToken ~ expP ~ expP ~ RightParenToken) ^^ { case _ ~ _ ~ e1 ~ e2 ~ _ => PlusExp(e1, e2) }
+  }
+}
